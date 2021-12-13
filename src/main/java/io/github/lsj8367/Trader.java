39eb4c25514c43a4a494286fc4fb231bf4e6a539
lsj8367 <lsj8367@naver.com>
2021-12-13 21:21:55 +0900

    Initial commit

---
+++ src/main/java/io/github/lsj8367/Trader.java
@@ -0,0 +1,25 @@
+package io.github.lsj8367;
+
+public class Trader {
+    private final String name;
+    private final String city;
+
+    public Trader(final String name, final String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    @Override
+    public String toString() {
+        return "Trader: " + name + " in " + city;
+    }
+
+}
---
+++ src/main/java/io/github/lsj8367/Transaction.java
@@ -0,0 +1,27 @@
+package io.github.lsj8367;
+
+public class Transaction {
+
+    private final Trader trader;
+    private final int year;
+    private final int value;
+
+    public Transaction(final Trader trader, final int year, final int value) {
+        this.trader = trader;
+        this.year = year;
+        this.value = value;
+    }
+
+    public Trader getTrader() {
+        return trader;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+}
